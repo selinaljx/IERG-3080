17593209e74d6e4c7f942ab63d463dafcbb6abe5
selinaljx <selinalaishe1118@gmail.com>
2014-02-01 04:50:36 +0800

    Update SavingAccount.cs

--- Assignment3/SavingAccount.cs
+++ Assignment3/SavingAccount.cs
@@ -7,18 +7,19 @@ namespace Assignment_3
 {
     class SavingAccount : Account
     {
-        public string Region { get; private set; }
-        public string Branch { get; private set; }
         public string ID { get; private set; }
-        public string Balance { get; private set; }
+        public double Balance { get; private set; }
+        public string Type { get; private set; }
 
-        public SavingAccount(string region, string branch, string id, string balance) : base (region, branch, id, balance)
+
+        public SavingAccount(string id, string balance, string type) : base (id, balance, type)
         { }
 
         public override void Report()
         {
-            Console.Write("Saving account");
             base.Report();
         }
+
+       
     }
 }
