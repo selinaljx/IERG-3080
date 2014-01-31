ce2868e44c9b5fbfe0a460b25cecf85570aae785
selinaljx <selinalaishe1118@gmail.com>
2014-02-01 04:48:40 +0800

    Update MyReader.cs

--- Assignment3/MyReader.cs
+++ Assignment3/MyReader.cs
@@ -2,39 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
+using System.IO;  //to use the class of StreamReader
 
 namespace Assignment_3
 {
     class MyReader
     {
 
-        public List<Account> ImportAccount(string filename)
+        public Dictionary<string, List<Account>> ImportAccount(string filename)
         {
             //read the .csv file
             var reader = new StreamReader(File.OpenRead(filename));
-            List<Account> list = new List<Account>();
+            Dictionary<string, List<Account>> regionList = new Dictionary<string, List<Account>>();
 
+            
             while (!reader.EndOfStream)
             {
 
                 var line = reader.ReadLine();
                 var values = line.Split(',');
+                
+                string region = values[0];
+                
+                // check if there is different region code
+                if (!regionList.ContainsKey(region))
+                {
+                    regionList.Add(region, new List<Account>());
+                }
+
+                List<Account> accountList = regionList[values[0]];
 
                 foreach (var v in values)
                 {
-                    if (v == "Saving")
+                    if (v == "Saving" && values[0] != "Region")
                     {
-                        list.Add(new SavingAccount(values[0], values[1], values[2], values[4]));
+                        accountList.Add(new SavingAccount(values[0]+'-'+values[1]+'-'+values[2], values[4], "Saving"));
                     }
-                    else if (v == "Current")
+                    else if (v == "Current" && values[0] != "Region")
                     {
-                        list.Add(new CurrentAccount(values[0], values[1], values[2], values[4]));
+                        accountList.Add(new CurrentAccount(values[0]+'-'+values[1]+'-'+values[2], values[4], "Current"));
                     }
                 }
+
+                
             }
 
-            return list;
+            return regionList;
         }
     }
 }
