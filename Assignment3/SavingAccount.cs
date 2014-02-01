1dbc8607846af0d8b8fd912742bed3e46c3741eb
selinaljx <selinalaishe1118@gmail.com>
2014-02-02 02:50:00 +0800

    Update SavingAccount.cs

--- Assignment3/SavingAccount.cs
+++ Assignment3/SavingAccount.cs
@@ -7,6 +7,7 @@ namespace Assignment_3
 {
     class SavingAccount : Account
     {
+        private Tuple<double, double> Tuple;
         public string ID { get; private set; }
         public double Balance { get; private set; }
         public string Type { get; private set; }
@@ -19,7 +20,29 @@ namespace Assignment_3
         {
             base.Report();
         }
+        public double Withdraw(double amount, double balance)
+        {
+            return balance -= amount;
+        }
 
-       
+        public double Deposit(double amount, double balance)
+        {
+            return balance += amount;
+        }
+
+        public double[] Transfer(Assignment_3.CurrentAccount currentAccount, double amount, double save, double current)
+        {
+           // save -= amount;
+           // current = currentAccount.Deposit(amount, current);
+
+            double[] trans;
+            trans = new double[2];
+
+            trans[0] = save - amount;
+            trans[1] = currentAccount.Deposit(amount, current);
+
+            return trans;
+             
+        }
     }
 }
